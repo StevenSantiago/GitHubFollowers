cd6a977363550224b6ca350ca18e07eb46c3ce4b
Steven Santiago <sasantia93@gmail.com>
2020-03-07 21:03:04 -0500

    Adding viewcontrollers folder and first two VC

---
+++ GitHubFollowers/GitHubFollowers/View Controllers/SearchVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Steven Santiago on 3/7/20.
+//  Copyright © 2020 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
