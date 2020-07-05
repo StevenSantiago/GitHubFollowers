f98992a749abc4235c659b518a029755af557063
Steven Santiago <stevenalfred93@yahoo.com>
2020-07-04 20:10:20 -0700

    - Setup basics of custom collection view cell

---
+++ GitHubFollowers/GitHubFollowers/Views/AvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  AvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Steven Santiago on 7/4/20.
+//  Copyright © 2020 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class AvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
