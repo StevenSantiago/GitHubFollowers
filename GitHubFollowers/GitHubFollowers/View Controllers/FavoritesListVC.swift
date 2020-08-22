78e067617bb1a636fad096129e3316752f42db5f
Steven Santiago <stevenalfred93@yahoo.com>
2020-08-22 00:30:52 -0700

    - Added cell and table view to display favorites list

--- GitHubFollowers/GitHubFollowers/View Controllers/FavoritesListVC.swift
+++ GitHubFollowers/GitHubFollowers/View Controllers/FavoritesListVC.swift
@@ -9,12 +9,72 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
+    
+    let tableView = UITableView()
+    var favorites: [Follower] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        configureViewController()
+        configureTableView()
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.resuseID)
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let favorites):
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "No Favorites?/n Add one on the follower screen.", in: self.view)
+                } else {
+                    self.favorites = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        // may need to load table view to front
+                    }
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
 
+}
+
+extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.resuseID) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
+    
+    
 }
---
+++ GitHubFollowers/GitHubFollowers/Views/FavoriteCell.swift
@@ -0,0 +1,58 @@
+//
+//  FavoriteCell.swift
+//  GitHubFollowers
+//
+//  Created by Steven Santiago on 8/21/20.
+//  Copyright © 2020 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+    static let resuseID = "FavoriteCell"
+    
+    let avatarImageView = AvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fonttSize: 26)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init code not implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func set(favorite: Follower) {
+           usernameLabel.text = favorite.login
+           avatarImageView.downloadImage(from: favorite.avatarUrl)
+       }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType = .disclosureIndicator
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    
+}
